e603e692ac60e61560933d4037c4308df22687d8
Roman Milko <rvmilko@gmail.com>
2023-11-20 09:43:39 +0200

    Filters

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -8,15 +8,29 @@ use App\Models\Invoice;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\InvoiceResource;
 use App\Http\Resources\V1\InvoiceCollection;
+use App\Filters\V1\InvoicesFilter;
+use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
     /**
      * Display a listing of the resource.
+     * 
+     * @return App\Http\Responce
      */
-    public function index()
+    public function index(Request $request)
     {
-        return new InvoiceCollection(Invoice::paginate());
+        $filter = new InvoicesFilter();
+
+        $queryItems = $filter->transform($request);
+
+        if(count($queryItems) == 0) {
+            return new InvoiceCollection(Invoice::paginate());
+        } else {
+            $invoices = Invoice::where($queryItems)->paginate();
+            
+            return new InvoiceCollection($invoices->appends($request->query()));
+        }
     }
 
     /**
